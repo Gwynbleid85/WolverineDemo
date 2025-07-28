b590ba4fc2ca90060a37c52e4aabe9180abfa098
Milos Hegr <hegr.milos85@seznam.cz>
2025-07-28 11:50:52 +0200

    fix: delete command logic

--- src/Todos/Application/Commands/Todos/DeleteTodoCommand.cs
+++ src/Todos/Application/Commands/Todos/DeleteTodoCommand.cs
@@ -1,5 +1,6 @@
 using System.Net;
 using CleanResult;
+using Mapster;
 using Marten;
 using Todos.Core;
 
@@ -9,18 +10,21 @@ public record DeleteTodoCommand(Guid Id);
 
 public class DeleteTodoCommandHandler
 {
-    public static async Task<Result> LoadAsync(DeleteTodoCommand command, IQuerySession session)
+    public static async Task<Result<Todo>> LoadAsync(DeleteTodoCommand command, IQuerySession session)
     {
         var todo = await session.LoadAsync<Todo>(command.Id);
-        if (todo is not null)
+        if (todo is null)
             return Result.Error("Todo not found", HttpStatusCode.NotFound);
-        return Result.Ok();
+        return Result.Ok(todo);
     }
 
     public static async Task<Result> Handle(DeleteTodoCommand command, Todo todo, IDocumentSession session)
     {
         session.Delete(todo);
         await session.SaveChangesAsync();
+
+        var asdf = "asdf";
+        var asd = asdf.Adapt<Result>();
 
         return Result.Ok();
     }
